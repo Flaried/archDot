975ffae61c2bd86b9d4a6d7d7380a882b3d92b7b
Flaried <80365319+Flaried@users.noreply.github.com>
2024-09-27 13:12:26 -0400

    Add files via upload

---
+++ config/ytfzf/backup.conf.sh
@@ -0,0 +1,28 @@
+# This is a sample config file, refer to ytfzf(5) for more information
+
+# In the previous version of ytfzf this file had all the examples, with all defaults set,
+# this has been changed because it made it impossible for us to change default values that were broken or causing bugs,
+# as everyone used the default configuration file.
+# we are now going to only have this sample config file, and the ytfzf(5) manual, which has explanation of every variable and function that can be set.
+
+#a sample config below:
+async_thumbnails=1
+search_again=1
+search_sort_by="relevance"
+sub_link_count=1
+show_thumbnails=1
+is_sort=1
+pages_to_scrape=10
+pages_start=1
+preview_window_width=40
+max_thread_count=40 # default 20
+sub_link_count=60
+search_result_type="all"
+enable_submenus=1
+#fancy_subs=1
+is_loop=1
+region=81
+YTFZF_SUBSCRIPTIONS_FILE=$YTFZF_CONFIG_DIR/japanese
+invidious_instance="https://vid.puffyan.us"
+gap_space=600
+
